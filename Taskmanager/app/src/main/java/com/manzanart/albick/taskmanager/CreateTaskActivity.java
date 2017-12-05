28b1da5c8f638e2d4cc24f0fbaed7430c4f867c9
manzano alban <manzanoalban@gmail.com>
2017-12-05 11:50:47 +0100

    Change screen is up

---
+++ Taskmanager/app/src/main/java/com/manzanart/albick/taskmanager/CreateTaskActivity.java
@@ -0,0 +1,28 @@
+package com.manzanart.albick.taskmanager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by 4223 on 05/12/2017.
+ */
+
+
+    public class CreateTaskActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.add_task);
+
+             }
+
+    }
+
--- Taskmanager/app/src/main/java/com/manzanart/albick/taskmanager/DisplayTask.java
+++ Taskmanager/app/src/main/java/com/manzanart/albick/taskmanager/DisplayTask.java
@@ -1,5 +1,6 @@
 package com.manzanart.albick.taskmanager;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -26,12 +28,14 @@ public class DisplayTask extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(view.getContext(), CreateTaskActivity.class);
+                startActivity(i);
             }
         });
         mListView = (ListView) findViewById(R.id.list);
