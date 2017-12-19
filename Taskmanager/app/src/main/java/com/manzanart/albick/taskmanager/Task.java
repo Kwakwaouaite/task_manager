6ba27be824a80734ed6cf3399f2f01cc22a8d897
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2017-12-19 11:46:35 +0100

    Reset the color of the background and text, when status change

--- Taskmanager/app/src/main/java/com/manzanart/albick/taskmanager/Task.java
+++ Taskmanager/app/src/main/java/com/manzanart/albick/taskmanager/Task.java
@@ -145,10 +145,12 @@ public class Task implements  Serializable{
             Log.d("end", Long.toString(end));
             if (today > end) {
                 view.all.setBackgroundColor(Color.argb(100, 200, 10, 10));
+                view.timeLeft.setTextColor(0xff000000);
             }
             else{
                 float percent = (1-(end - today))/(end - start + 0.0000000001f);
                 view.timeLeft.setTextColor(Color.argb((int)(100*percent),200,50,50));
+                view.all.setBackgroundColor(0);
             }
 
         }
